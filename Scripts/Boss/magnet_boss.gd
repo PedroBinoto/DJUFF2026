1f1e9c7941133df081242ff97f5649f2ad7ecca8
Thiago Lelles Fernandes <thilelles@gmail.com>
2026-06-07 18:12:49 -0300

    Bugfix da altura do boss + Local da espada

--- Scripts/Boss/magnet_boss.gd
+++ Scripts/Boss/magnet_boss.gd
@@ -3,5 +3,10 @@ extends CharacterBody3D
 
 @export var healthComponent: HealthComponent = null
 
+func _physics_process(delta: float) -> void:
+	if !is_on_floor():
+		velocity.y -= 50 * delta
+	move_and_slide()
+
 func _process(delta: float) -> void:
 	pass
