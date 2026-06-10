d26a01e088a5960e26f587b222db54c84f9300d3
supermeganato <renatovm@dcc.ufrj.br>
2026-06-09 22:43:35 -0300

    ADD: esteira implementada

---
+++ Scripts/esteira.gd
@@ -0,0 +1,20 @@
+extends Area3D
+
+@export var linear_velocity: Vector3 = Vector3.LEFT * PI
+var player: Node3D
+
+func _ready() -> void:
+	$Mesh.get_active_material(0).set_shader_parameter("Speed", sign(linear_velocity.x) * .4)
+
+func _on_body_entered(body: Node3D) -> void:
+	player = body
+	player.create_force(self, Vector3.ZERO)
+
+func _process(_delta: float) -> void:
+	if (player):
+		var force = linear_velocity.rotated(Vector3.UP, rotation.y)
+		player.update_force(self, force)
+
+func _on_body_exited(_body: Node3D) -> void:
+	player.delete_force(self)
+	player = null
