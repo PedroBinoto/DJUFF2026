d82f56352aa2011fefd22c3cf45adb926e84eeb4
Thiago Lelles Fernandes <thilelles@gmail.com>
2026-06-11 00:58:10 -0300

    ULTIMO COMMIT DA JAM GLORIA

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -34,6 +34,7 @@ func _on_resume_pressed() -> void:
 	pauseMenu()
 
 func _on_quit_pressed() -> void:
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/hub.tscn")
 
 func _on_button_2_pressed() -> void:
